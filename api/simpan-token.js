d5937c1fa67ff244e455b8ab11b368c9b8ac78af
Koteka74 <133244579+Koteka74@users.noreply.github.com>
2025-05-25 13:35:22 +0900

    Add files via upload

---
+++ api/simpan-token.js
@@ -0,0 +1,27 @@
+// api/simpan-token.js
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Metode tidak diizinkan" });
+  }
+
+  const { token } = req.body;
+
+  if (!token) {
+    return res.status(400).json({ message: "Token kosong" });
+  }
+
+  try {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbzEmuQwEYw5NBeq3va03WnegvIy0Ef2ZQbONaZI_M8Uxuqkakw_fbsD2T0QkLltP_Y/exec", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ token })
+    });
+
+    const result = await response.json();
+    return res.status(200).json({ result: "success", data: result });
+  } catch (err) {
+    return res.status(500).json({ result: "error", message: err.message });
+  }
+}
