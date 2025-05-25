75cdec1dc28ac7b6293665dc93676104d3412f9d
Koteka74 <133244579+Koteka74@users.noreply.github.com>
2025-05-25 20:37:35 +0900

    Update kirim-notifikasi.js new deploy

--- api/kirim-notifikasi.js
+++ api/kirim-notifikasi.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   }
 
   // 1. Fetch tokens dari Google Sheet
-  const sheetUrl = "https://script.google.com/macros/s/AKfycbz1hPIaD2WvlBLlsQRRKq15lJqIB62WOsMyB8q7e5_a9ltAkS2I3NeklaKNOwpV1iE/exec"; // ganti dengan URL Apps Script Anda
+  const sheetUrl = "https://script.google.com/macros/s/AKfycbxPDtLvX7OAMwObKluHEZQJxGnKy5YDfV8w4Ot9JVj6c7JiN49fPaAy6wXLpScNRW4/exec"; // ganti dengan URL Apps Script Anda
   let tokenList = [];
 
   try {
