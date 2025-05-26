b38c6712d8145345d15fdd3c85279588ccf5820c
Koteka74 <133244579+Koteka74@users.noreply.github.com>
2025-05-26 14:37:50 +0900

    Update firebase-token.js ganti fungsi ambil token

--- firebase-token.js
+++ firebase-token.js
@@ -12,25 +12,29 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function ambilToken() {
-  const messaging = firebase.messaging();
+  navigator.serviceWorker.ready.then((registration) => {
+    const messaging = firebase.messaging();
 
-  messaging
-    .getToken({
-      vapidKey: "BKEmOESDGJWvHc5qSlctfHN_b5Z56eIOkUJCMXw70h0BV5Og0xWs6OkSv_rfnSIeLCMDntWjKI6HrMs6-pSD4gw"
-    })
-    .then((currentToken) => {
-      if (currentToken) {
-        console.log("✅ Token FCM:", currentToken);
-        simpanToken(currentToken);
-        localStorage.setItem("fcm_token", currentToken);
-      } else {
-        console.warn("⚠️ Token kosong.");
-      }
-    })
-    .catch((err) => {
-      console.error("❌ Gagal ambil token:", err);
-    });
+    messaging
+      .getToken({
+        vapidKey: "BKEmOESDGJWvHc5qSlctfHN_b5Z56eIOkUJCMXw70h0BV5Og0xWs6OkSv_rfnSIeLCMDntWjKI6HrMs6-pSD4gw",
+        serviceWorkerRegistration: registration
+      })
+      .then((currentToken) => {
+        if (currentToken) {
+          console.log("✅ Token FCM:", currentToken);
+          simpanToken(currentToken);
+          localStorage.setItem("fcm_token", currentToken);
+        } else {
+          console.warn("⚠️ Token kosong.");
+        }
+      })
+      .catch((err) => {
+        console.error("❌ Gagal ambil token:", err);
+      });
+  });
 }
+
 
 function simpanToken(token) {
   fetch("/api/simpan-token", {
