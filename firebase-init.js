61f6e780aea6cd50697ce72fd0b7c899f6a45a00
Koteka74 <133244579+Koteka74@users.noreply.github.com>
2025-05-31 15:24:11 +0900

    Update firebase-init.js ganti isi

--- firebase-init.js
+++ firebase-init.js
@@ -1,4 +1,7 @@
 // firebase-init.js
+import "https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js";
+import "https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCmvG_P7ekN3Vn2lrM6xp7fE2F0NC_y0MA",
   authDomain: "transaksi-bakso-garasi.firebaseapp.com",
@@ -7,5 +10,18 @@ const firebaseConfig = {
   appId: "1:273103643750:web:bcba66f7fdb425d8ebf9a7"
 };
 
-// Inisialisasi Firebase
 firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onMessage((payload) => {
+  console.log("📥 Pesan diterima di foreground:", payload);
+
+  if (Notification.permission === 'granted') {
+    const { title, body } = payload.notification;
+    new Notification(title, {
+      body,
+      icon: "/icons/icon-192.png"
+    });
+  }
+});
