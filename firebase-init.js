837fd43a431b1cc12876ea2c83a102d8809c4e9a
Koteka74 <133244579+Koteka74@users.noreply.github.com>
2025-05-31 16:19:06 +0900

    Update firebase-init.js kembalikan ke semula

--- firebase-init.js
+++ firebase-init.js
@@ -1,6 +1,4 @@
-// firebase-init.js (versi final yang tidak pakai import)
-
-// Muat Firebase compat dari CDN
+// firebase-init.js
 const firebaseConfig = {
   apiKey: "AIzaSyCmvG_P7ekN3Vn2lrM6xp7fE2F0NC_y0MA",
   authDomain: "transaksi-bakso-garasi.firebaseapp.com",
@@ -9,18 +7,5 @@ const firebaseConfig = {
   appId: "1:273103643750:web:bcba66f7fdb425d8ebf9a7"
 };
 
+// Inisialisasi Firebase
 firebase.initializeApp(firebaseConfig);
-
-const messaging = firebase.messaging();
-
-// Tampilkan notifikasi saat browser aktif (foreground)
-messaging.onMessage((payload) => {
-  console.log("📥 Pesan diterima di foreground:", payload);
-  const { title, body } = payload.notification;
-  if (Notification.permission === "granted") {
-    new Notification(title, {
-      body,
-      icon: "/icons/icon-192.png"
-    });
-  }
-});
